5ae091aa4bb45d5559d1663a92556eac07d14041
Mattias Bonte <wisecodr@gmail.com>
2020-08-19 15:38:55 +0200

    Edit Folder Name

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './scripts/index.js',
+  entry: './js/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
